cb70ce054c3299001e515563982aed8544c08f32
Michael <onisoris@yandex.ru>
2024-05-28 15:20:21 +0300

    modify init file

--- ThreeDTool/__init__.py
+++ ThreeDTool/__init__.py
@@ -1,1 +1,11 @@
-from threeDTool import *
+from .threeDTool import *
+from .curve import *
+from .display import *
+from .layer import *
+from .line import *
+from .parser_stl import *
+from .plane import *
+from .polyhedron import *
+from .surface import *
+from .twoDTool import *
+import trimesh
