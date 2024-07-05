ffae8fca98ed9f5da4ce5537bd2cb3ad9cb86d1d
Michael <onisoris@yandex.ru>
2024-07-05 22:59:05 +0300

    Исправление тестового файла

--- ThreeDTool/tests/test_Polygon.py
+++ ThreeDTool/tests/test_Polygon.py
@@ -12,9 +12,12 @@ class TestPolygon:
         pol2 = Polygon(np.array([[1.5, 0, 0], [1.5, 1, 0], [0.5, 1, 0], [0.5, 0, 0]]))
         assert not pol1.polygons_equal(pol2)
 
-    def test_points_from_polygon_polygon_intersection(self):
-        pol1 = Polygon(np.array([[0, 0, 1], [1, 0, 1], [1, 1, 1], [0, 1, 1]]))
-        pol2 = Polygon(np.array([[1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 0, 0]]))
-        points = pol1.points_from_polygon_polygon_intersection(pol2)
-        print(points)
-        assert points
+    def test_points_from_polygon_polygon_intersection_and_rot_v(self):
+        from ..threeDTool import rot_v
+        pol1 = Polygon(np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]]))
+        points_pol = np.array([[1.5, 0, 0], [1.5, 1, 0], [0.5, 1, 0], [0.5, 0, 0]])
+        axis = [1, 1, 1]
+        points_pol = rot_v(points_pol, np.pi / 6, axis)
+        pol2 = Polygon(points_pol)
+        points_test = np.array([[1., 0.73205081, 0.], [0.6830127, 0.5, 0.]])
+        assert np.allclose(pol1.points_from_polygon_polygon_intersection(pol2), points_test, 1e-8)
