8bce193a79ea7f3aa2aafd474fee689155baf931
Michael <onisoris@yandex.ru>
2024-07-08 22:59:21 +0300

    parse_stl перенесен в threeDTool

--- ThreeDTool/threeDTool.py
+++ ThreeDTool/threeDTool.py
@@ -980,6 +980,46 @@ def rectangle_from_three_points(point1: list | ndarray, point2: list | ndarray, 
     return Polygon(vertices)
 
 
+def parse_stl(file):
+    """
+    Функция для парсинга STL файла
+    :param file: файл
+    :type file: str
+    :return: ndarray, str
+    """
+    text = file.read()
+    index_space = text.find(" ")
+    first_n = text.find("\n")
+    name = text[index_space + 1:first_n]
+    triangles = text.split("facet normal")[1:]
+    triangles_array = np.array([])
+
+    for id, triangle in enumerate(triangles):
+        t = triangle.split("\n")
+        normals_text = t[0][1:].split(" ")
+        normal = np.array([])
+
+        for norm in normals_text:
+            normal = np.hstack([normal, float(norm)])
+
+        vertex = t[2:5]
+        vertex_array = normal
+
+        for vert in vertex:
+            index = vert.find("vertex") + 9
+            coordinates = np.array(vert[index:].split(" "))
+            coordinates = coordinates.astype(float)
+            vertex_array = np.vstack([vertex_array, coordinates])
+        vertex_array = np.array([vertex_array])
+
+        if id == 0:
+            triangles_array = vertex_array
+        else:
+            triangles_array = np.vstack([triangles_array, vertex_array])
+
+    return triangles_array, name
+
+
 class Fvec:
     def __init__(self, xyz: ndarray | list, vector: ndarray | list):
         xyz = np.array(xyz)
