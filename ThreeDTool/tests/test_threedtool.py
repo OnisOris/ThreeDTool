0598336ae9a5b53c4ff38dbc29630980a50a88cc
Vlad Butkovskii <vtb170605@gmail.com>
2024-08-05 15:13:18 +0300

    Созданы тесты для методов создания прямоугольников, добавлены новые тесты проверки многоугольников на равенство

--- ThreeDTool/tests/test_threedtool.py
+++ ThreeDTool/tests/test_threedtool.py
@@ -58,10 +58,11 @@ class TestThreedtool:
         vector7 = [-1.44646, 1.673453536, 365.456772]
         vector8 = [1.4353672, -1.67345 * 256, 53455.456772484]
         var2 = [collinearity_vectors(vector1, vector2),
-               collinearity_vectors(vector3, vector4),
-               collinearity_vectors(vector5, vector6),
-               collinearity_vectors(vector7, vector8)]
+                collinearity_vectors(vector3, vector4),
+                collinearity_vectors(vector5, vector6),
+                collinearity_vectors(vector7, vector8)]
         assert np.all(var) and not np.all(var2)
+
     def test_collinearity_vectors_list(self):
         vector1 = [19, 19, 1]
         vector2 = [-38, -38, -2]
@@ -157,3 +158,76 @@ class TestThreedtool:
         line = Line(1, -1, 0, 1, -1, 0)
         point = point_from_line_segment_intersection(line, segment)
         assert np.allclose(point, [0., 0., 0.], 1e-8)
+
+    # rectangle_from_three_points ---------------------------
+    def test_rectangle_from_three_points_1(self):
+        polygon_1 = Polygon(np.array([[0, 0, 0], [2, 0, 0], [2, 2, 0], [0, 2, 0]]))
+        polygon_2 = rectangle_from_three_points([0, 0, 0], [2, 0, 0], [0, 2, 0])
+        assert polygon_1.polygons_equal(polygon_2)
+
+    def test_rectangle_from_three_points_2(self):
+        polygon_1 = Polygon(np.array([[2, 1, 0], [5, -1, 0], [1, -7, 0], [-2, -5, 0]]))
+        polygon_2 = rectangle_from_three_points([2, 1, 0], [5, -1, 0], [-3.95, -3.7, 0])
+        assert polygon_1.polygons_equal(polygon_2)
+
+    def test_rectangle_from_three_points_3(self):
+        polygon_1 = Polygon(np.array([[-10, 8.6, 2.4], [7.2, 8.6, 2.4], [7.2, -2.8, -9.4], [-10, -2.8, -9.4]]))
+        polygon_2 = rectangle_from_three_points([7.2, 8.6, 2.4], [-10, 8.6, 2.4], [18, -2.8, -9.4])
+        assert polygon_1.polygons_equal(polygon_2)
+
+    def test_rectangle_from_three_points_4(self):
+        polygon_1 = Polygon(np.array([[-3.6, -7.2, 4.5], [9.3, 5.5, -2.5], [5.43199016, 3.4200798, -13.40175907],
+                                      [-7.46800984, -9.2799202, -6.40175907]]))
+        polygon_2 = rectangle_from_three_points([9.3, 5.5, -2.5], [5.43199016, 3.4200798, -13.40175907],
+                                                [-3.6, -7.2, 4.5])
+        assert polygon_1.polygons_equal(polygon_2)
+
+        # incorrect
+
+    def test_rectangle_from_three_points_5(self):
+        polygon_1 = Polygon(np.array([[0, 0, 0], [2, 0, 0], [2, 2, 1], [0, 2, 1]]))
+        polygon_2 = rectangle_from_three_points([0, 0, 0], [2, 0, 0], [0, 2, 0])
+        assert not polygon_1.polygons_equal(polygon_2)
+
+    def test_rectangle_from_three_points_6(self):
+        polygon_1 = Polygon(np.array([[0, 0, 0], [2, 0, 0], [2, 2, 0], [0, 2, 0]]))
+        polygon_2 = rectangle_from_three_points([0, 0, 0], [2, 0, 0], [0, 2, 0])
+        assert polygon_1.polygons_equal(polygon_2)
+
+    def test_rectangle_from_three_points_7(self):
+        polygon_1 = Polygon(np.array([[0, 0, 0], [2, 0, 0], [2, 2, 0], [0, 2, 0]]))
+        polygon_2 = rectangle_from_three_points([0, 0, 0], [2, 0, 0], [1, 1, 0])
+        assert not polygon_1.polygons_equal(polygon_2)
+
+    # rectangle_from_center ---------------------------
+    def test_rectangle_from_center_1(self):
+        polygon_1 = Polygon(np.array([[5, 1, 0], [-5, 1, 0], [-5, -1, 0], [5, -1, 0]]))
+        polygon_2 = rectangle_from_center([0, 0, 0], 10, 2, [1, 0, 0], [0, 0, 1])
+        assert polygon_1.polygons_equal(polygon_2)
+
+    def test_rectangle_from_center_2(self):
+        polygon_1 = Polygon(np.array([[2, 1, 0], [5, -1, 0], [1, -7, 0], [-2, -5, 0]]))
+        polygon_2 = rectangle_from_center([1.5, -3, 0], 3.605551275464, 7.211102550928, [-3, 2, 0], [0, 0, 1])
+        assert polygon_1.polygons_equal(polygon_2)
+
+
+    def test_rectangle_from_center_3(self):
+        polygon_1 = Polygon(np.array([[-3.6, -7.2, 4.5], [9.3, 5.5, -2.5], [5.43199016, 3.4200798, -13.40175907],
+                                      [-7.46800984, -9.2799202, -6.40175907]]))
+        polygon_2 = rectangle_from_center([0.91599508, -1.8899601, -4.45087954], 19.4087609, 11.7531238,
+                                          [4.99501731, 4.91757518, -2.71047451], [-4.03467954, 4.42221572, 0.5878248])
+        assert polygon_1.polygons_equal(polygon_2)
+
+        # incorrect
+
+    def test_rectangle_from_center_4(self):
+        try:
+            polygon_1 = Polygon(np.array([[0, 0, 0], [2, 0, 0], [2, 2, 0], [0, 2, 0]]))
+            polygon_2 = rectangle_from_center([1, 1, 0], 1, 2, [1, 0, 0], [2, 3, 1])
+        except(ValueError): assert True
+        else: assert False
+
+    def test_rectangle_from_center_5(self):
+        polygon_1 = Polygon(np.array([[0, 0, 0], [2, 0, 0], [2, 2, 0], [0, 2, 0]]))
+        polygon_2 = rectangle_from_center([1, 1, 0], 1, 2, [1, 0, 0], [0, 1, 0])
+        assert not polygon_1.polygons_equal(polygon_2)
